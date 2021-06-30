99c909acc1c5e6eede34f88d81fb08ef14c2bf4b
colin-combe <colin.combe@ed.ac.uk>
2021-06-30 12:47:17 +0100

    ug, merge

--- src/crosslink-viewer-BB.js
+++ src/crosslink-viewer-BB.js
@@ -3,6 +3,7 @@ import Backbone from "backbone";
 import {RenderedProtein} from "./interactor/rendered-protein";
 import {RenderedCrosslink} from "./link/rendered-crosslink";
 import {P_PLink} from "./link/p_p-link";
+import {utils} from "../../xi3/js/utils";
 
 import {Group} from "./interactor/group";
 
@@ -351,23 +352,23 @@ export class CrosslinkViewer extends Backbone.View{
     }
 
     zoomToFullExtent () {
-        // // this.container.setAttribute("transform", "scale(1)");
-        // const width = this.svgElement.parentNode.clientWidth;
-        // const height = this.svgElement.parentNode.clientHeight;
-        // const bbox = this.container.getBBox();
-        // let xr = (width / bbox.width).toFixed(4);
-        // let yr = (height / bbox.height).toFixed(4);
-        // let scaleFactor;
-        // if (yr < xr) {
-        //     scaleFactor = yr;
-        // } else {
-        //     scaleFactor = xr;
-        // }
-        // // if (scaleFactor > 1) {
-        // //     scaleFactor = scaleFactor / 0.8;
+        // this.container.setAttribute("transform", "scale(1)");
+        const width = this.svgElement.parentNode.clientWidth;
+        const height = this.svgElement.parentNode.clientHeight;
+        const bbox = this.container.getBBox();
+        let xr = (width / bbox.width).toFixed(4);
+        let yr = (height / bbox.height).toFixed(4);
+        let scaleFactor;
+        if (yr < xr) {
+            scaleFactor = yr;
+        } else {
+            scaleFactor = xr;
+        }
+        // if (scaleFactor > 1) {
+        //     scaleFactor = scaleFactor / 0.8;
         // // }
-        // this.container.setAttribute("transform", "scale(" + scaleFactor + ") translate(" + ((width / scaleFactor) - bbox.width - bbox.x) + " " + -bbox.y + ")");
-        // this.scale();
+        this.container.setAttribute("transform", "scale(" + scaleFactor + ") translate(" + ((width / scaleFactor) - bbox.width - bbox.x) + " " + -bbox.y + ")");
+        this.scale();
     }
 
     scale () {
